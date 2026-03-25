78794e6a0162002acb0243d0b4a41c438ef137ac
Richard Buehling <richardbuehling@Richards-Mac-mini.local>
2026-03-25 04:42:37 -0500

    Update to v0.3.0

--- Formula/benmore.rb
+++ Formula/benmore.rb
@@ -1,13 +1,13 @@
 class Benmore < Formula
   desc "Turn HTML + SQL into secure web apps"
   homepage "https://benmore.ai"
-  version "0.1.0"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Benmore-Studio/benmore-releases/releases/download/v0.1.0/benmore-darwin-arm64.tar.gz"
-      sha256 "6767af7fac0cff11bd1f531bcae66ca4c84ed32a4eb7c89226ea5534ae8eb143"
+      url "https://github.com/Benmore-Studio/benmore-releases/releases/download/v0.3.0/benmore-darwin-arm64.tar.gz"
+      sha256 "da2f688e2a9c28e6a1e2ce0f2fa1a17c65b1c23486465a4209c08cb758ced310"
     end
   end
 
@@ -18,6 +18,6 @@ class Benmore < Formula
   end
 
   test do
-    assert_match "benmore 0.1.0", shell_output("#{bin}/benmore version")
+    assert_match "benmore 0.3.0", shell_output("#{bin}/benmore version")
   end
 end
