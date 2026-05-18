f17d1abc84cab832a7603b7a6387aebc7b982402
Richard Buehling <richard@benmore.tech>
2026-05-18 07:41:06 -0500

    Update to v2.2.5

--- Formula/benmore.rb
+++ Formula/benmore.rb
@@ -1,13 +1,13 @@
 class Benmore < Formula
   desc "The web framework for AI-driven development"
   homepage "https://benmore.ai"
-  version "2.2.4"
+  version "2.2.5"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Benmore-Studio/benmore-releases/releases/download/v2.2.4/benmore-darwin-arm64.tar.gz"
-      sha256 "e0a159ee096233153a511f91deb4f7552b68111ee24ae11338d215847d6736b0"
+      url "https://github.com/Benmore-Studio/benmore-releases/releases/download/v2.2.5/benmore-darwin-arm64.tar.gz"
+      sha256 "ab6b602af80ef151fd0bc0f71a25f817c9a22063a3262eb84e5425980cda59e9"
     end
   end
 
@@ -18,6 +18,6 @@ class Benmore < Formula
   end
 
   test do
-    assert_match "benmore 2.2.4", shell_output("#{bin}/benmore version")
+    assert_match "benmore 2.2.5", shell_output("#{bin}/benmore version")
   end
 end
